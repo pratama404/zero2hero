81548520dede7d9cd1f38538651ca09ca2dc157a
pratama404 <agengputrapratama@gmail.com>
2024-11-21 12:53:43 +0700

    add to github

---
+++ app/metadata.tsx
@@ -0,0 +1,6 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Geotera",
+    description: "AI-powered waste management platform",
+  };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env:{
+        DATABASE_URL: process.env.DATABASE_URL,
+        WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID,
+        NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+        NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+
+    },
+};
 
 export default nextConfig;
